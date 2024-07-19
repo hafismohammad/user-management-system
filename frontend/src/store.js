293dc6d5255004055af37b781d50013751c3f208
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2024-07-19 10:37:47 +0530

    project initial setup done

---
+++ frontend/src/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+const rootReducer = combineReducers({
+    // here reducer
+})
+
+const store = createStore()
